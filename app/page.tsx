b14050b89e766067e98630e409f8ed9746e4af28
HagarNaim1994 <noamhagar6@gmail.com>
2025-09-01 13:51:48 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+return (
+<main style={{height:'100vh', width:'100vw', margin:0, padding:0}}>
+<iframe
+src="/site/index.html"
+title="Nova-style Site"
+style={{border:'none', width:'100%', height:'100%'}}
+/>
+</main>
+)
+}
